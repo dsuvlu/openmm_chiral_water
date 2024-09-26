e0c8beea4959ee62f702896af9b40ee248a90532
dsuvlu <dsuvlu@gmail.com>
2024-09-26 10:24:05 -0400

    update

--- openmm_chial_water/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This repo is an example workflow for performing simulations of chiral small molecules in water using OpenMM."""
-
-# Add imports here
-from .setup import *
-
-
-from ._version import __version__
